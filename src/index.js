6bfbd977a8800743d63580b6c2ca605cb397ee2c
Adrienne <adriennepaquin@gmail.com>
2021-05-21 16:40:34 -0400

    finished and some extras

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#create-task-form").addEventListener("submit", (e) =>{
     e.preventDefault();
   
-    let eventObj = {description: e.target["new-task-description"].value}
+    let eventObj = {
+      description: e.target["new-task-description"].value,
+      date: e.target["due-date"].value,
+      priority: e.target["priority"].value
+    }
     appendList(eventObj);
+
 
   })
 
@@ -20,20 +25,32 @@ function appendList(object) {
   let ul = document.querySelector("#tasks");
   let deleteBtn = document.createElement("button");
   deleteBtn.textContent = "x"
-  deleteBtn.className = "delete-button"
-  
+  // deleteBtn.className = "delete-button"
+  // deleteBtn.id = `${object.description}Btn`
+  deleteBtn.addEventListener("click", (e) =>{
+    console.log(e)
+    e.target.parentElement.remove()
+  })
+  li.style.color = 'white'
+  if (object.priority === "red"){
+    li.style.background = 'red'
+  } else if (object.priority === "yellow"){
+    li.style.background = 'yellow'
+    li.style.color = 'black'
+  } else {
+    li.style.background = 'green'
+  }
 
   li.className = "task-list";
+  li.id = `${object.description}Id`
 
-  li.textContent = object.description;
+  li.textContent = object.description + ": " + object.date;
   li.append(deleteBtn);
 
   ul.append(li);
   console.log(ul);
 }
 
-document.querySelector(".delete-button").addEventListener("click", (e) => {
-  document.querySelector(".task-list").remove();
-})
+
 
 
