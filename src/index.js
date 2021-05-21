279e593936119f13deac0f1fb558dce3fde72377
jackcornblum <jcornblum@gmail.com>
2021-05-21 15:35:37 -0400

    implemented task adding feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", (e) =>{
+    e.preventDefault();
+  
+    let eventObj = {description: e.target["new-task-description"].value}
+    appendList(eventObj);
+
+  })
+
+  
+
+
 });
+
+let test = {description: "value here"};
+
+function appendList(object) {
+  let li = document.createElement("li");
+  let ul = document.querySelector("#tasks");
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "x"
+  deleteBtn.className = "delete-button"
+  
+
+  li.className = "task-list";
+
+  li.textContent = object.description;
+  li.append(deleteBtn);
+
+  ul.append(li);
+  console.log(ul);
+}
+
+document.querySelector(".delete-button").addEventListener("click", (e) => {
+  document.querySelector(".task-list").remove();
+})
+
+
